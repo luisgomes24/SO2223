a5a2387a5e5a1835b23c6339f60b0d8ce44d2cfb
Luis Fernandes Gomes <lfgomes@andrew.cmu.edu>
2022-10-18 09:23:33 +0100

    TP4

--- TP4/mycat.c
+++ TP4/mycat.c
@@ -8,29 +8,26 @@
 int main(int argc, char *argv[])
 {
     /* check if exactly one argument is present */
-    if (argc < 2)
+    if (argc != 2)
     {
         printf("usage: cat filename\n");
         return EXIT_FAILURE;
     }
     /* check if file can be opened and is readable */
-    for (int i = 1; i < argc; i++)
+    int fd = open(argv[1], O_RDONLY);
+    if (fd == -1)
     {
-        int fd = open(argv[i], O_RDONLY);
-        if (fd == -1)
-        {
-            printf("error: cannot open %s\n", argv[i]);
-            return EXIT_FAILURE;
-        }
-        /* print the contents in blocks */
-        char buffer[BUFFER_SIZE];
-        int bytesin;
-        while ((bytesin = read(fd, buffer, BUFFER_SIZE)) > 0)
-        {
-            write(STDOUT_FILENO, buffer, bytesin);
-        }
-        /* close file */
-        close(fd);
+        printf("error: cannot open %s\n", argv[1]);
+        return EXIT_FAILURE;
     }
+    /* print the contents in blocks */
+    char buffer[BUFFER_SIZE];
+    int bytesin;
+    while ((bytesin = read(fd, buffer, BUFFER_SIZE)) > 0)
+    {
+        write(STDOUT_FILENO, buffer, bytesin);
+    }
+    /* close file */
+    close(fd);
     return EXIT_SUCCESS;
 }
